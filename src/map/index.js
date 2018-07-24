1fb53412e111b7bf5cfb6ba047ae67c10f3297b5
Lucien Perouze <lucienperouze@gmail.com>
2018-07-24 18:29:00 +0200

    Lat + Long ok on the map

--- src/map/index.js
+++ src/map/index.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+
 import './style.css';
 
 class Map extends Component {
+
+    componentDidMount() {
+        $('.map').click(function (e) {
+            const pixX = e.pageX - $(this).position().left;
+            const pixY = e.pageY - $(this).position().top;
+
+            const percX = pixX / $(this).width();
+            const percY = pixY / $(this).height();
+
+            const lat = (percY - 0.5) * -180;
+            const long = (percX - 0.5) * 360;
+
+            console.log(lat, long);
+        });
+    }
+
     render() {
         return (
             <div className={'map'}>
             </div>
         );
     }
+
 }
 
 export default Map;
